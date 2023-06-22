b117c3dc95060ed1810b08982de4f756ed6512e1
Karolina Sadlik <truskaweczka518@gmail.com>
2023-06-22 17:15:33 +0200

    Add Cloud Firestore and StreamBuilder

--- lib/home/home_page_context.dart
+++ lib/home/home_page_context.dart
@@ -1,4 +1,6 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({
@@ -8,34 +10,59 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.black,
       floatingActionButton: FloatingActionButton(
         onPressed: () {},
         child: const Icon(Icons.add),
       ),
       appBar: AppBar(
-        title: const Center(
+        title: Center(
             child: Text(
           "Dzień tygodnia",
-          style: TextStyle(color: Colors.white),
+          style: GoogleFonts.alkatra(fontSize: 30, color: Colors.white),
         )),
         backgroundColor: Colors.purple,
       ),
-      body: ListView(
-        children: [
-          Container(
-            decoration: BoxDecoration(
-              border: Border.all(
-                color: Colors.purple,
-              ),
-              borderRadius: const BorderRadius.all(
-                Radius.circular(20),
-              ),
-            ),
-            margin: const EdgeInsets.all(20),
-            padding: const EdgeInsets.all(20),
-            child: const Center(child: Text('PONIEDZIAŁEK')),
-          ),
-        ],
+      body: StreamBuilder<QuerySnapshot>(
+          stream: FirebaseFirestore.instance.collection('lessons').snapshots(),
+          builder: (context, snapshot) {
+            return ListView(
+              children: const [
+                WeekDayContainer('poniedziałek'),
+                WeekDayContainer('wtorek'),
+                WeekDayContainer('sroda'),
+              ],
+            );
+          }),
+    );
+  }
+}
+
+class WeekDayContainer extends StatelessWidget {
+  const WeekDayContainer(
+    this.weekday, {
+    super.key,
+  });
+
+  final String weekday;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        border: Border.all(
+          color: Colors.purple,
+        ),
+        borderRadius: const BorderRadius.all(
+          Radius.circular(20),
+        ),
+      ),
+      margin: const EdgeInsets.all(20),
+      padding: const EdgeInsets.all(20),
+      child: Center(
+        child: Text(
+          weekday,
+          style: GoogleFonts.alkatra(fontSize: 20, color: Colors.white),
+        ),
       ),
     );
   }
