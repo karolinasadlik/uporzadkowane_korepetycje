1c739cd10b273f2733579aa58ace26c8a91db578
Karolina Sadlik <truskaweczka518@gmail.com>
2023-07-25 21:37:53 +0200

    Add Auth Gate to the project

--- lib/home/auth/auth_gate.dart
+++ lib/home/auth/auth_gate.dart
@@ -1,27 +1,25 @@
-
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutterfire_ui/auth.dart';
+import 'package:firebase_auth/firebase_auth.dart' as auth;
 import 'package:flutter/material.dart';
 import 'package:uporzadkowane_korepetycje/home/home_page_context.dart';
+import 'package:firebase_ui_auth/firebase_ui_auth.dart';
 
 class AuthGate extends StatelessWidget {
   const AuthGate({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return StreamBuilder<User?>(
-      stream: FirebaseAuth.instance.authStateChanges(),
-      builder: (context, snapshot) {
-        if (!snapshot.hasData) {
-          return const SignInScreen(
-            providerConfigs: [
-              EmailProviderConfiguration(),
-            ],
-          );
-        }
+    return StreamBuilder<auth.User?>(
+        stream: auth.FirebaseAuth.instance.authStateChanges(),
+        builder: (context, snapshot) {
+          if (!snapshot.hasData) {
+            return SignInScreen(
+              providers: [
+                EmailAuthProvider(),
+              ],
+            );
+          }
 
-        return HomePage();
-      },
-    );
+          return HomePage();
+        });
   }
 }
